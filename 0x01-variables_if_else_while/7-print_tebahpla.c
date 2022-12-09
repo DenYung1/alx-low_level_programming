e40b7e8dfc53ad43add6131f6355aab3a8b08d1e
Dennis <dennisabayateye@gmail.com>
2022-12-09 06:58:01 -0800

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Reverse Alphabets
+ * Return: Always 0
+ */
+intr main(void)
+{
+	char dennis;
+
+	for (dennis = 'z'; dennis >= 'a'; dennis--)
+	{
+		putchar(dennis);
+	}
+	putchar('\n');
+	return (0);
+}
