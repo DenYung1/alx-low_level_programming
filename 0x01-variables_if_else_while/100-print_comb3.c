815969ec8cee3a009c538e2bcb25232071f3b5bd
Dennis <dennisabayateye@gmail.com>
2022-12-09 08:23:33 -0800

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - print all possible different combination of two digits
+ * Return: Always 0
+ */
+int main(void)
+{
+	int w;
+
+	for (w = '0'; d < 100; w++)
+	{
+		putchar((w / 10) + '0');
+		putchar((d % 10) + '0');
+		if (w != 99)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
