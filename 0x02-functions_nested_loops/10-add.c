6e6b0c8cce6589d160b2b666a51e982af6132e3f
Dennis <dennisabayateye@gmail.com>
2022-12-13 12:53:59 -0800

    Why

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - functions that add two integers and returns the result
+ * @w: - integer w
+ * @y: - integer y
+ * Description: add two integers and returns the result
+ * Result: add w and y
+ */
+int add(int w, int y)
+{
+	return (w + y);
+}
